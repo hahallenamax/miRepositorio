e3855e753b93853d5a8a965080a34b559232ce7c
AulaUCJC <AulaUCJC@192.168.164.138>
2024-04-03 10:16:47 +0200

    ª

---
+++ src/primero/MiClaseDePrueba.java
@@ -0,0 +1,12 @@
+package primero;
+
+public class MiClaseDePrueba {
+public static void main(String[] args) {
+	helloworld("System.out.println()");
+}
+
+
+static void helloworld(String text) {
+	System.out.println(text);
+}
+}
